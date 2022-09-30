36e3fcf8e6691aabf66e168b50e7415cd0a90dfe
Ivan Kalinin <i.kalinin@dexpa.io>
2022-09-30 19:02:55 +0200

    Add provider selector

---
+++ src/components/ProviderSelector.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{
+  providers: { title: string; object: any }[];
+  onSelect: (provider: any) => void;
+}>();
+</script>
+
+<template>
+  <div class="modal is-active">
+    <div class="modal-background" />
+
+    <div class="modal-content select-provider-modal">
+      <div class="box">
+        <p class="is-size-5 mb-3">Connect a wallet</p>
+        <div class="buttons">
+          <button
+            class="button is-fullwidth"
+            v-for="(provider, i) in providers"
+            :key="i"
+            @click="onSelect(provider.object)"
+          >
+            {{ provider.title }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.select-provider-modal {
+  width: min(20rem, 90vw) !important;
+}
+</style>
