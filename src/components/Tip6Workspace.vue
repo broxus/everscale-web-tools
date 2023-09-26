c4ccf21ed4b403949a5fd65570db107a1c0e7b8f
Pavel Ivanov <sudokey@gmail.com>
2023-09-26 11:52:18 +0300

    Added tip6 page

---
+++ src/components/Tip6Workspace.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { ref, shallowRef, watchEffect } from 'vue';
+
+const input = ref<string>('');
+const selectedMethods = ref<string[]>([])
+const state = shallowRef<{
+  error?: undefined;
+  methods: string[];
+}>({
+  methods: [],
+});
+
+watchEffect(() => {
+  try {
+    const parsed = JSON.parse(input.value || '{}');
+    const methods = parsed.functions?.map(item => item.name) ?? []
+
+    selectedMethods.value = []
+    state.value = {
+      methods,
+    }
+  } catch (e: any) {
+    state.value = {
+      error: e.toString(),
+      methods: [],
+    };
+  }
+});
+
+function toggleAll() {
+  selectedMethods.value = selectedMethods.value.length !== state.value.methods.length
+    ? state.value.methods
+    : []
+}
+</script>
+
+<template>
+  <section class="section pb-0">
+    <div class="container is-fluid">
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">Enter JSON ABI:</label>
+            <div class="control mb-3">
+              <textarea
+                spellcheck="false"
+                rows="5"
+                :class="['textarea', { 'is-danger': state.error != null }]"
+                v-model="input"
+                ></textarea>
+            </div>
+
+            <pre v-if="state.error != null" class="help is-danger">{{ state.error }}</pre>
+          </div>
+
+          <div class="field" v-if="state.methods.length > 0">
+            <div class="label is-flex is-justify-content-space-between is-align-content-center">
+              Methods:
+
+              <button
+                class="button is-small"
+                :disabled="state.methods.length === 0"
+                @click="toggleAll()"
+              >
+                Toggle all
+              </button>
+            </div>
+
+            <div
+              class="mb-3"
+              v-for="method in state.methods"
+            >
+              <label class="button is-fullwidth is-justify-content-flex-start">
+                <div class="checkbox">
+                  <input
+                    type="checkbox"
+                    :id="method"
+                    :value="method"
+                    v-model="selectedMethods"
+                  /> {{ method }}
+                </div>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <label class="label">Interface ID:</label>
+          <pre class="encoded-data">Interface Id</pre>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
